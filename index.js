9a23733f651d1397216edc35b0741246123ce9f1
woliart <artwol993@gmail.com>
2022-02-11 15:59:30 +0100

    add websocket config

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app);
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on('connection', function connection(ws) {
+  console.log('A new client Connected!')
+  ws.send('Welcome new Client');
+  ws.on('message', function message(message) {
+    console.log('received: %s', message);
+    ws.send('Got ur msg its' + message)
+  });
+
+
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
